7ee9451d17150a7d0fd886b6f52b969890f9e9c8
vitamin777 <rlaalsdn8@gmail.com>
2022-09-18 20:19:00 +0900

    feat : 상세 보기 클릭시 상세보기 페이지로 이동 설정
    -MeetupItem.js 에서 useRouter 를 활용해서 router.push method 로 url 뒤에 MeetupList.js 에서 목록을 출력할때 넘어온 id 를 탐색하게 해서 상세 보기 페이지를 출력하도록함
    
    -기능 정상 작동 확인

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,7 +1,18 @@
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
+import { useRouter } from 'next/router';
 
 function MeetupItem(props) {
+  const router = useRouter();
+
+  const showDetailsHandler = () => {
+    /*프로그래밍 방식으로 탐색하는 메소드 push()
+     * -새 페이지를 페이지 더미에 연결하며 Link 컴포넌트를 사용하는 것이나 마찬가지
+     * -모임 목록을 렌더링할 때 ID 프로퍼티를 MeetupItem 으로 보냈기에
+     * --meetupId는 프로퍼티로 읽어 들였었고 MeetupItem 대신에 props.id를 이용해서 동적 경로를 지정*/
+    router.push('/' + props.id);
+  };
+
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +24,7 @@ function MeetupItem(props) {
           <address>{props.address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={showDetailsHandler}>상세 보기</button>
         </div>
       </Card>
     </li>
